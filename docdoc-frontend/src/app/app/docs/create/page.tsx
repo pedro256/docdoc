b21718a17fd8329956441fe9fdaf2a2c58ee40be
Pedro Henrique <pedromartins@lampp-it.com.br>
2025-02-25 11:12:22 -0400

    fix exemplo de hook form em editText

--- docdoc-frontend/src/app/app/docs/create/page.tsx
+++ docdoc-frontend/src/app/app/docs/create/page.tsx
@@ -12,17 +12,25 @@ import {
 } from "@react-pdf/renderer";
 import "./styles.css";
 import TextEditor from "@/components/text-editor/TextEditor";
-import { useState } from "react";
+// import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+interface ITest {
+  content?: string;
+}
 
 export default function CreateNewDocument() {
-  const [document, setDocument] = useState<string>();
+  const { control, handleSubmit } = useForm<ITest>();
   return (
     <main className="w-full">
       <div className="flex w-full">
         <div className="w-2/3 p-2">
           <div className="flex gap-4 items-center">
             <IoDocumentTextOutline className="w-12 h-12" />
-            <Input className="font-bold" placeholder="Documento Tets 1" />
+            <Input
+              className="text-2xl font-bold"
+              placeholder="Documento Tets 1"
+            />
             <FaRegFloppyDisk className=" text-green-600 w-8 h-8 opacity-75" />
           </div>
 
@@ -33,18 +41,33 @@ export default function CreateNewDocument() {
             <div>Visualizar</div>
             <div>Ajuda</div>
           </div>
-          <div className="editor_area_items">
-            <TextEditor />
+          <div>
+            <form onSubmit={handleSubmit((data) => console.log(data))}>
+              <div className="editor_area_items">
+                <Controller
+                  control={control}
+                  name="content"
+                  render={({ field }) => (
+                    <TextEditor
+                      content={field.value}
+                      onChange={field.onChange}
+                    />
+                  )}
+                />
+              </div>
+              <button type="submit">OK</button>
+            </form>
           </div>
+
           {/* <code className="text-xs bg-gray-900 text-white py-1 px-2 rounded">
             {document}
           </code> */}
         </div>
         <div className="w-1/3 border relative">
           <h1 className="text-center text-sm py-2">Pré-visualização</h1>
-          {/* <PDFViewer style={{ width: "100%", height: "500px" }}>
+          <PDFViewer style={{ width: "100%", height: "500px" }}>
             <MyDocument text={"texto qualquer"} />
-          </PDFViewer> */}
+          </PDFViewer>
         </div>
       </div>
     </main>
