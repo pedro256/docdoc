816d335a40e86381484c66ce8de8b26fec054d82
Pedro Henrique <pedromartins@lampp-it.com.br>
2025-02-28 10:52:40 -0400

    models

---
+++ docdoc-frontend/src/models/context/DocBase/DocBaseModel.ts
@@ -0,0 +1,11 @@
+export default class DocBaseModel{
+    id:string;
+    docType:string;
+    title:string;
+
+    constructor(){
+        this.id="";
+        this.title="";
+        this.docType="";
+    }
+}
