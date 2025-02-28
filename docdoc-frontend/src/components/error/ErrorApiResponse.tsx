4341bec1de80e7f4aa451d9233296968b2fddbe8
Pedro Henrique <pedromartins@lampp-it.com.br>
2025-02-28 10:51:06 -0400

    component infor error search

---
+++ docdoc-frontend/src/components/error/ErrorApiResponse.tsx
@@ -0,0 +1,15 @@
+import { MdErrorOutline } from "react-icons/md";
+
+export default function ErrorApiResponse() {
+  return (
+    <div>
+      <div className="flex items-center gap-4 ">
+        <MdErrorOutline className="w-10 h-10"/>
+        <div>
+          <h2>ERRO DE RESPOSTA</h2>
+          <span className="opacity-75">Erro na busca de dados servidor</span>
+        </div>
+      </div>
+    </div>
+  );
+}
